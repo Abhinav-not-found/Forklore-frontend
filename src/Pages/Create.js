5b8864fe7a04d53b88f8b426642005b54e77949f
abhinav <abhinavisgreat18@gmail.com>
2024-07-24 12:49:39 -0700

    making create page

--- src/Pages/Create.js
+++ src/Pages/Create.js
@@ -2,8 +2,22 @@ import React from 'react'
 
 const Create = () => {
     return (
-        <div>
-            Create Page
+        <div className='h-full'>
+            <p className='text-xl'>Title</p>
+            <input className='text-2xl px-1 outline-none bg-transparent border border-black rounded-md mb-5' type="text"/>
+
+            <p className='text-xl'>Short description</p>
+            <textarea cols='40' rows='5' className='text-2xl px-1 outline-none bg-transparent border border-black rounded-md mb-5'></textarea>
+
+            <p className='text-xl'>Image</p>
+            <input className='text-2xl px-1 outline-none bg-transparent border border-black rounded-md mb-5' type="text"/>
+
+            <p className='text-xl'>Ingredients</p>
+            <textarea cols='60' rows='10' className='text-2xl px-1 outline-none bg-transparent border border-black rounded-md mb-5'></textarea>
+
+            <p className='text-xl'>Recipe</p>
+            <textarea cols='60' rows='10' className='text-2xl px-1 outline-none bg-transparent border border-black rounded-md mb-5'></textarea>
+            
         </div>
     )
 }
